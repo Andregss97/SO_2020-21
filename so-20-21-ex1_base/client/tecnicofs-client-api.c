f0a92a3d559058ccf28878d170b68db01fcf3fe2
Andregss97 <andregoncalo97@hotmail.com>
2020-11-27 15:58:53 +0000

    server test (lab)

---
+++ so-20-21-ex1_base/client/tecnicofs-client-api.c
@@ -0,0 +1,90 @@
+#include "tecnicofs-client-api.h"
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <stdio.h>
+
+#define CLIENT "/tmp/client"
+
+socklen_t servlen, clilen;
+struct sockaddr_un serv_addr, client_addr;
+char buffer[1024];
+int sockfd;
+
+int setSockAddrUn(char *path, struct sockaddr_un *addr) {
+
+  if (addr == NULL)
+    return 0;
+
+  bzero((char *)addr, sizeof(struct sockaddr_un));
+  addr->sun_family = AF_UNIX;
+  strcpy(addr->sun_path, path);
+
+  return SUN_LEN(addr);
+}
+
+int tfsCreate(/*char *filename, char nodeType*/const char *mensagem) {
+
+  if (sendto(sockfd, mensagem, strlen(mensagem)+1, 0, (struct sockaddr *) &serv_addr, servlen) < 0) {
+      perror("client: sendto error");
+      exit(EXIT_FAILURE);
+  } 
+
+  if (recvfrom(sockfd, buffer, sizeof(buffer), 0, 0, 0) < 0) {
+      perror("client: recvfrom error");
+      exit(EXIT_FAILURE);
+  } 
+
+  printf("Recebeu resposta do servidor: %s\n", buffer);
+
+  return EXIT_SUCCESS;
+}
+
+int tfsDelete(char *path) {
+  return -1;
+}
+
+int tfsMove(char *from, char *to) {
+  return -1;
+}
+
+int tfsLookup(char *path) {
+  return -1;
+}
+
+int tfsMount(char * sockPath) {
+
+  /*if (argc < 3) {
+    printf("Argumentos esperados:\n path_client_socket path_server_socket string_a_enviar\n");
+    return EXIT_FAILURE;
+  }*/
+
+  if ((sockfd = socket(AF_UNIX, SOCK_DGRAM, 0) ) < 0) {
+    perror("client: can't open socket");
+    exit(EXIT_FAILURE);
+  }
+
+  unlink(CLIENT);
+  clilen = setSockAddrUn (CLIENT, &client_addr);
+  if (bind(sockfd, (struct sockaddr *) &client_addr, clilen) < 0) {
+    perror("client: bind error");
+    exit(EXIT_FAILURE);
+  }  
+
+  servlen = setSockAddrUn(sockPath, &serv_addr);
+
+  return EXIT_SUCCESS;
+}
+
+int tfsUnmount() {
+  if(!sockfd){
+    return EXIT_FAILURE;
+  }
+  close(sockfd);
+
+  unlink(CLIENT);
+ 
+  return EXIT_SUCCESS;
+}
--- so-20-21-ex1_base/main.c
+++ so-20-21-ex1_base/main.c
@@ -3,6 +3,12 @@
 #include <getopt.h>
 #include <pthread.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <sys/stat.h>
 #include <sys/time.h>
 #include <ctype.h>
 #include "tecnicofs-api-constants.h"
@@ -11,242 +17,182 @@
 #define MAX_COMMANDS 150000
 #define MAX_INPUT_SIZE 100
 #define MAX_LOCKS 50        //same as inode_table size
+#define SERVER "/tmp/server"
+#define INDIM 30
+#define OUTDIM 512
 
 int numberThreads = 0;
-
-char inputCommands[MAX_COMMANDS][MAX_INPUT_SIZE];
-int numberCommands = 0;
-int headQueue = 0;
-
-int insertCommand(char* data) {
-    if(numberCommands != MAX_COMMANDS) {
-        strcpy(inputCommands[numberCommands++], data);
-        return 1;
-    }
-    return 0;
-}
-
-char* removeCommand() {
-    if(numberCommands > 0){
-        numberCommands--;
-        return inputCommands[headQueue++];  
-    }
-    return NULL;
-}
 
 void errorParse(){
     fprintf(stderr, "Error: command invalid\n");
     exit(EXIT_FAILURE);
 }
 
-void processInput(char* filename){
-    char line[MAX_INPUT_SIZE];
+void* applyCommands(char* command){
 
-    FILE *file = fopen(filename,"r");
-    if( file == NULL ) {
-        fprintf(stderr, "Error: no such file\n");
+    char type, token;
+    char name[MAX_INPUT_SIZE];
+    char file1[MAX_INPUT_SIZE];
+    char file2[MAX_INPUT_SIZE];
+    int numTokens;
+    pthread_rwlock_t *rwl;
+
+    int* buffer_locks = malloc(MAX_LOCKS * sizeof(int));
+    int i = 0;
+    int* count = &i;
+
+    if (command[0] == 'm') {
+        numTokens = sscanf(command, "%c %s %s", &token, file1, file2);
+    }
+    else {
+        numTokens = sscanf(command, "%c %s %c", &token, name, &type);
+    }
+
+    // numTokens = sscanf(command, "%c %s %c", &token, name, &type);
+
+    if (numTokens < 2) {
+        fprintf(stderr, "Error: invalid command in Queue\n");
         exit(EXIT_FAILURE);
     }
-    /* break loop with ^Z or ^D */
-    while (fgets(line, sizeof(line)/sizeof(char), file)) {
-        char type, token;
-        char name[MAX_INPUT_SIZE];
-        char file1[MAX_INPUT_SIZE];
-        char file2[MAX_INPUT_SIZE];
-        int numTokens;
-        if (strcmp(&line[0], "m") == 0) {
-            numTokens = sscanf(line, "%c %s %s", &token, file1, file2);
-        }
-        else {
-            numTokens = sscanf(line, "%c %s %c", &token, name, &type);
-        }
-        /* perform minimal validation */
-        if (numTokens < 1) {
-            continue;
-        }
 
-        switch (token) {
-            case 'c':
-                if(numTokens != 3){
-                    printf("C erro: %d\n", numTokens);
-                    errorParse();
-                }
-                if(insertCommand(line))
+    //int searchResult;
+    switch (token) {
+        case 'c':
+
+            switch (type) {
+                case 'f':
+                        
+                    //printf("Create file: %s\n", name);
+                    printf("CREATE: buffer %d %d %d %d %d\n", buffer_locks[0], buffer_locks[1], buffer_locks[2], buffer_locks[3], buffer_locks[4]);
+                    create(name, T_FILE, buffer_locks);
                     break;
-                return;
-            
-            case 'm':
-                if(numTokens != 3){
-                    printf("M erro: %d\n", numTokens);
-                    errorParse();
-                }
-                if(insertCommand(line))
+                case 'd':
+                        
+                    //printf("Create directory: %s\n", name);
+                    printf("CREATE: buffer %d %d %d %d %d\n", buffer_locks[0], buffer_locks[1], buffer_locks[2], buffer_locks[3], buffer_locks[4]);
+                    create(name, T_DIRECTORY, buffer_locks);
                     break;
-                return;
-            
-            case 'l':
-                if(numTokens != 2){
-                    printf("L erro: %d\n", numTokens);
-                    errorParse();
-                }
-                if(insertCommand(line))
-                    break;
-                return;
-            
-            case 'd':
-                if(numTokens != 2){
-                    printf("D erro: %d\n", numTokens);
-                    errorParse();
-                }
-                if(insertCommand(line))
-                    break;
-                return;
-            
-            case '#':
-                break;
-            
-            default: { /* error */
-                errorParse();
+                default:
+                    fprintf(stderr, "Error: invalid node type\n");
+                    exit(EXIT_FAILURE);
             }
-        }
-    }
-    fclose(file);
-}
+            break;
+        case 'l':
+            
+            printf("LOOKUP: buffer %d %d %d %d %d\n", buffer_locks[0], buffer_locks[1], buffer_locks[2], buffer_locks[3], buffer_locks[4]);
+            lookup(name, buffer_locks, LOOKUP, count);
 
-void* applyCommands(){
+            for (int i=0; i < *count; i++){
+                inode_get_lock(buffer_locks[i], &rwl);
+                pthread_rwlock_unlock(rwl);
+                printf("\t[%ld] Buffer index: %d // UnLock no iNode: %d\n", pthread_self(), i, buffer_locks[i]);
+            }
 
-    while (numberCommands > 0){
+            printf("[%ld] DOES FREE lookup\n", pthread_self());
+            free(buffer_locks);
 
-        const char* command = removeCommand();
+                
+            //if (searchResult >= 0)
+                //printf("Search: %s found\n", name);
+            //else
+                //printf("Search: %s not found\n", name);
+            
+            break;
+        
+        case 'd':
 
-        if (command == NULL){
-            continue;
-        }
+            //printf("Delete: %s\n", name);
+            printf("DELETE: buffer %d %d %d %d %d\n", buffer_locks[0], buffer_locks[1], buffer_locks[2], buffer_locks[3], buffer_locks[4]);
+            delete(name, buffer_locks);
 
-        char type, token;
-        char name[MAX_INPUT_SIZE];
-        char file1[MAX_INPUT_SIZE];
-        char file2[MAX_INPUT_SIZE];
-        int numTokens;
-        pthread_rwlock_t *rwl;
+            break;
 
-        int* buffer_locks = malloc(MAX_LOCKS * sizeof(int));
-        int i = 0;
-        int* count = &i;
-
-        if (command[0] == 'm') {
-            numTokens = sscanf(command, "%c %s %s", &token, file1, file2);
-        }
-        else {
-            numTokens = sscanf(command, "%c %s %c", &token, name, &type);
-        }
-
-        // numTokens = sscanf(command, "%c %s %c", &token, name, &type);
-
-        if (numTokens < 2) {
-            fprintf(stderr, "Error: invalid command in Queue\n");
+        case 'm':
+            move(file1, file2, buffer_locks);
+            break;
+            
+        default: { /* error */
+            fprintf(stderr, "Error: command to apply\n");
             exit(EXIT_FAILURE);
         }
-
-        //int searchResult;
-        switch (token) {
-            case 'c':
-
-                switch (type) {
-                    case 'f':
-                         
-                        //printf("Create file: %s\n", name);
-                        printf("CREATE: buffer %d %d %d %d %d\n", buffer_locks[0], buffer_locks[1], buffer_locks[2], buffer_locks[3], buffer_locks[4]);
-                        create(name, T_FILE, buffer_locks);
-                        break;
-                    case 'd':
-                         
-                        //printf("Create directory: %s\n", name);
-                        printf("CREATE: buffer %d %d %d %d %d\n", buffer_locks[0], buffer_locks[1], buffer_locks[2], buffer_locks[3], buffer_locks[4]);
-                        create(name, T_DIRECTORY, buffer_locks);
-                        break;
-                    default:
-                        fprintf(stderr, "Error: invalid node type\n");
-                        exit(EXIT_FAILURE);
-                }
-                break;
-            case 'l':
-                
-                printf("LOOKUP: buffer %d %d %d %d %d\n", buffer_locks[0], buffer_locks[1], buffer_locks[2], buffer_locks[3], buffer_locks[4]);
-                lookup(name, buffer_locks, LOOKUP, count);
-
-                for (int i=0; i < *count; i++){
-                    inode_get_lock(buffer_locks[i], &rwl);
-                    pthread_rwlock_unlock(rwl);
-                    printf("\t[%ld] Buffer index: %d // UnLock no iNode: %d\n", pthread_self(), i, buffer_locks[i]);
-                }
-
-                printf("[%ld] DOES FREE lookup\n", pthread_self());
-                free(buffer_locks);
-
-                 
-                //if (searchResult >= 0)
-                    //printf("Search: %s found\n", name);
-                //else
-                    //printf("Search: %s not found\n", name);
-                
-                break;
-            
-            case 'd':
-
-                //printf("Delete: %s\n", name);
-                printf("DELETE: buffer %d %d %d %d %d\n", buffer_locks[0], buffer_locks[1], buffer_locks[2], buffer_locks[3], buffer_locks[4]);
-                delete(name, buffer_locks);
-
-                break;
-
-            case 'm':
-                move(file1, file2, buffer_locks);
-                break;
-                
-            default: { /* error */
-                fprintf(stderr, "Error: command to apply\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-        // printf("DOES FREE");
-    
     }
+    // printf("DOES FREE");
     return NULL;
 }
 
-void verify_inputs(int argc, char* argv[]) {
-    if (argc != 4) {
-        fprintf(stderr, "Error: invalid number of arguments\n");
-        exit(EXIT_FAILURE);
-    }
-    else if (!isalpha(*argv[1])) {
-        fprintf(stderr, "Error: the given inputfile is not a char\n");
-        exit(EXIT_FAILURE);
-    }
-    else if (!isalpha(*argv[2])) {
-        fprintf(stderr, "Error: the given outputfile is not a char\n");
-        exit(EXIT_FAILURE);
-    }
-    else if (!isdigit(*argv[3])) {
-        fprintf(stderr, "Error: the number of threads must be a positive integer\n");
-        exit(EXIT_FAILURE);
-    }
+int setSockAddrUn(char *path, struct sockaddr_un *addr) {
+
+  if (addr == NULL)
+    return 0;
+
+  bzero((char *)addr, sizeof(struct sockaddr_un));
+  addr->sun_family = AF_UNIX;
+  strcpy(addr->sun_path, path);
+
+  return SUN_LEN(addr);
 }
 
 int main(int argc, char* argv[]) {
+    int sockfd;
+    struct sockaddr_un server_addr;
+    socklen_t addrlen;
+    char *path;
+
     struct timeval t0, t1, totalT;
     /* init filesystem */
     init_fs();
     /* process input and print tree */
-    verify_inputs(argc, argv);
+    
     /* argv[1] aka inputfile given as atribute */
-    processInput(argv[1]);
-    numberThreads = atoi(argv[3]);
-    pthread_t tid[numberThreads];
+    numberThreads = atoi(argv[1]);
+    //pthread_t tid[numberThreads];
+
+    if (argc < 2)
+    exit(EXIT_FAILURE);
+
+    if ((sockfd = socket(AF_UNIX, SOCK_DGRAM, 0)) < 0) {
+        perror("server: can't open socket");
+        exit(EXIT_FAILURE);
+    }
+
+    path = SERVER;
+
+    unlink(path);
+
+    addrlen = setSockAddrUn (SERVER, &server_addr);
+    if (bind(sockfd, (struct sockaddr *) &server_addr, addrlen) < 0) {
+        perror("server: bind error");
+        exit(EXIT_FAILURE);
+    }
+    
+    while (1) {
+        struct sockaddr_un client_addr;
+        char in_buffer[INDIM], out_buffer[OUTDIM];
+        int c;
+
+        addrlen=sizeof(struct sockaddr_un);
+        c = recvfrom(sockfd, in_buffer, sizeof(in_buffer)-1, 0,
+            (struct sockaddr *)&client_addr, &addrlen);
+        if (c <= 0) continue;
+        //Preventivo, caso o cliente nao tenha terminado a mensagem em '\0', 
+        in_buffer[c]='\0';
+        
+        printf("Recebeu mensagem de %s\n", client_addr.sun_path);
+
+        c = sprintf(out_buffer, "Ola' %s, que tal vai isso?", in_buffer);
+        
+        sendto(sockfd, out_buffer, c+1, 0, (struct sockaddr *)&client_addr, addrlen);
+
+    }
+
+    //Fechar e apagar o nome do socket, apesar deste programa 
+    //nunca chegar a este ponto
+    close(sockfd);
+    unlink(SERVER);
+    exit(EXIT_SUCCESS);
 
     gettimeofday(&t0, NULL);
-    for (int i=0; i < numberThreads; i++) {
+    /*for (int i=0; i < numberThreads; i++) {
         if (pthread_create(&(tid[i]), NULL, applyCommands, NULL) != 0) {
             printf("Error creating thread.\n");
             return -1;
@@ -254,11 +200,12 @@ int main(int argc, char* argv[]) {
     }
 
     for (int i=0; i < numberThreads; i++) {
+        printf("THREAD NUMBER [%d]: %ld\n\n\n", i, tid[i]);
         if(pthread_join(tid[i], NULL) != 0) {
             printf("Error joining thread.\n");
             return -1;
         }
-    }
+    }*/
     gettimeofday(&t1, NULL);
     timersub(&t1, &t0, &totalT);
 
